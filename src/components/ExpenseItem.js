c3daa30f624bdaa6b77095ad0a8a9ffb096777e7
vikas9620 <vikassingh9620@gmail.com>
2023-05-03 16:34:16 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,9 @@
+function ExpenseItem(){
+    return(<div>
+        <h1>Expense Items</h1>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p> Movies Rs 200</p></div>
+    )
+}
+export default ExpenseItem
