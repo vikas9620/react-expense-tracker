471860c935936437c6cbc9e62aa72df1424ec166
vikas9620 <vikassingh9620@gmail.com>
2023-05-04 17:04:08 +0530

    first commit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,13 +6,14 @@ import ExpenseDetails from './ExpenseDetails';
 
 const ExpenseItem=(props)=>{
    
+   
     return(
     
     <Card className='expense-item'>
   
     <ExpenseDate Date={props.Date} />
     <ExpenseDetails amount={props.amount}  LocationOfExpenditure={props.LocationOfExpenditure}   title={props.title} />
-    
+    <button>Delete Expense</button>
     </Card>
   
     )
