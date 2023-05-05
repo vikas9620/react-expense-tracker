b03350e0bc7dbfed2afa21d20037a12430beaa97
vikas9620 <vikassingh9620@gmail.com>
2023-05-05 14:20:37 +0530

    added multiple state in expenseform

--- src/components/ExpenseFrom/ExpenseForm.js
+++ src/components/ExpenseFrom/ExpenseForm.js
@@ -1,12 +1,26 @@
 
 import './ExpenseForm.css'
-
+import React,{useState} from 'react';
 
 
 const ExpenseForm= ()=>{
+const [EnterTitle,setEnterTitle]=useState('');
+const [EnterLocation,setEnterLocation]=useState('');
+const [EnterAmount,setEnterAmount]=useState('');
+const [EnterDate,setEnterDate]=useState('');
 
-const titleHandler= (event)=>{
-    console.log(event.target.value);
+
+const titleChangeHandler= (event)=>{
+    setEnterTitle(event.target.value);
+}
+const locationChangeHandler= (event)=>{
+    setEnterLocation(event.target.value);
+}
+const amountChangeHandler= (event)=>{
+    setEnterAmount(event.target.value);
+}
+const dateChangeHandler= (event)=>{
+    setEnterDate(event.target.value);
 }
 
 
@@ -15,13 +29,13 @@ const titleHandler= (event)=>{
         <h1>Expense Form</h1>
         <div className="form">
         <label className="text">Title</label>
-        <input type="text" placeholder="title" onChange={titleHandler} required ></input>
+        <input type="text" placeholder="title" onChange={titleChangeHandler} required ></input>
         <label className="text">Location</label>
-        <input type="text" placeholder="location" required ></input>
+        <input type="text" placeholder="location" onChange={locationChangeHandler} required ></input>
         <label className="text">Amount</label>
-        <input type="number" placeholder="amount" required ></input>
+        <input type="number" placeholder="amount" onChange={amountChangeHandler} required ></input>
         <label className="text">Date</label>
-        <input type="Date" placeholder="Date" required ></input>
+        <input type="Date" placeholder="Date" onChange={dateChangeHandler} required ></input>
         
         </div><button type="submit" className="btn">submit</button>
         </div>
