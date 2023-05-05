dcc4ff60ae778641a552885292940abde82f504d
vikas9620 <vikassingh9620@gmail.com>
2023-05-05 17:28:09 +0530

    form submit

--- src/components/ExpenseFrom/ExpenseForm.js
+++ src/components/ExpenseFrom/ExpenseForm.js
@@ -4,6 +4,8 @@ import React,{useState} from 'react';
 
 
 const ExpenseForm= ()=>{
+   // const [userInput,setUserInput]=useState({EnterTitle: '',
+//EnterLocation: '',EnterAmount: '',EnterDate: ''});
 const [EnterTitle,setEnterTitle]=useState('');
 const [EnterLocation,setEnterLocation]=useState('');
 const [EnterAmount,setEnterAmount]=useState('');
@@ -12,20 +14,35 @@ const [EnterDate,setEnterDate]=useState('');
 
 const titleChangeHandler= (event)=>{
     setEnterTitle(event.target.value);
+   // setUserInput({...userInput,EnterTitle: event.target.value});
+//setUserInput((preState)=>{
+   // return ({...preState,EnterTitle: event.target.value})});
 }
 const locationChangeHandler= (event)=>{
     setEnterLocation(event.target.value);
+    //setUserInput({...userInput,EnterLocation: event.target.value});
 }
 const amountChangeHandler= (event)=>{
     setEnterAmount(event.target.value);
+    //setUserInput({...userInput,EnterAmount: event.target.value});
 }
 const dateChangeHandler= (event)=>{
     setEnterDate(event.target.value);
+   // setUserInput({...userInput,EnterDate: event.target.value});
+}
+const SubmitHandler= (event)=>{
+    event.preventDefault();
+    const expenseData = {
+        title: EnterTitle,
+        location: EnterLocation,
+        amount: EnterAmount,
+        date: new Date(EnterDate)
+    }
+    console.log(expenseData);
 }
 
-
     return(
-        <div className="expense-form">
+        <form className="expense-form" onSubmit={SubmitHandler}>
         <h1>Expense Form</h1>
         <div className="form">
         <label className="text">Title</label>
@@ -37,8 +54,8 @@ const dateChangeHandler= (event)=>{
         <label className="text">Date</label>
         <input type="Date" placeholder="Date" onChange={dateChangeHandler} required ></input>
         
-        </div><button type="submit" className="btn">submit</button>
-        </div>
+        </div><button type="submit" className="btn">Add Expense</button>
+        </form>
     )
 }
 export default ExpenseForm
