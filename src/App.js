6aade2fe53be3232ee585fa2dce31fca0bda26a7
vikas9620 <vikassingh9620@gmail.com>
2023-05-06 13:20:25 +0530

    add expenseFilter

--- src/App.js
+++ src/App.js
@@ -1,47 +1,50 @@
-import ExpenseItem from "./components/Expenses/ExpenseItem";
+import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/ExpenseFrom/NewExpense";
-import React, { useState } from "react";
+import React from "react";
 const App = (props) => {
- /* const expenses1 = [
+  const expenses1 = [
     {
+      id: "e1",
       title: "food",
-      amount: 8678,
-      date: new Date(2023, 1, 1),
-      location: "noida",
-    }
-    
-  ];*/
-  const [expenses, setExpenses] = useState([]);
+      amount: 342,
+      location: "noidan",
+      date: new Date(2021, 7, 15),
+    },
+    { id: "e2", title: "trip", amount: 578,location: "noidan", date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "travel",
+      amount: 54,
+      location: "noidan",
+      date: new Date(2020, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "tour",
+      amount: 8767,
+      location: "noidan",
+      date: new Date(2021, 5, 12),
+    },
+  ];
+  //const [expenses, setExpenses] = useState([]);
 
   const addExpenseHandler = (expenseData) => {
-    const newExpense = {
-      ...expenseData
-    };
-
-    setExpenses((prevExpenses) => {
-      return [...prevExpenses, newExpense];
-    });
+   // const newExpense = {
+  //    ...expenseData,};
+    console.log(expenseData)
+    //setExpenses((prevExpenses) => {
+      //return [...prevExpenses, newExpense]; });
   };
-  const renderExpenses = () => {
-    if (expenses.length === 0) {
-      return <p>No expenses found.</p>;
-    }
-  };
+  //const renderExpenses = () => {
+   // if (expenses.length === 0) {
+   //   return <p>No expenses found.</p>;
+   // }
+ // };
   return (
     <div className="App">
       <NewExpense onAddExpense={addExpenseHandler} />
       <h1>Expense Items</h1>
-      {renderExpenses()}
-      <header className="App-header">
-        {expenses.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-            location={expense.location}
-          />
-        ))}
-      </header>
+      <Expenses items={expenses1} />
     </div>
   );
 };
--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,31 +1,18 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 
 const ExpenseDetails = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const [amount, setAmount] = useState(props.amount);
-  const clickHandler1 = () => {
-    setTitle("title");
-  };
-  const clickHandler2 = () => {
-    setAmount("100");
-  };
-  const location = props.location;
+ 
   return (
     <div className="expense-description">
       <div className="expense-item2">
-        <div>{title}</div>
-        <button onClick={clickHandler1} className="button2">
-          update title
-        </button>
+        <div>{props.title}</div>
       </div>
       <div className="expense-amount">
-        <div className="expense-price">${amount}</div>
+        <div className="expense-price">${props.amount}</div>
       </div>
-      <button onClick={clickHandler2} className="button1">
-        update amount
-      </button>
-      <div className="location-of-expenditure">{location}</div>
+     
+      <div className="location-of-expenditure">{props.location}</div>
     </div>
   );
 };
